cad20632186d69943b9e951ea1b422eccbe7aea3
mattroweshow <m.rowe@lancaster.ac.uk>
2015-04-09 16:48:27 +0100

    Updated Model Test with Items Cats SVD Setting

--- examples/model_test.py
+++ examples/model_test.py
@@ -2,6 +2,7 @@ from dataset.recsys_dataset_loader import recsys_loader
 from dataset.dataset_splitter import datasetsplitter
 from models.svd import SVD
 from models.svdplusplus import SVDPlusPlus
+from models.items_cats_svd import ItemsCatsSVD
 from learning.sgd_learner import SGD
 import os
 
@@ -11,13 +12,20 @@ params = {"eta": 0.1, "lambda": 0.1, "f": 10, "max_epochs": 100, "epsilon": 0.00
 
 working_dir = os.path.dirname(os.path.realpath(__file__))
 path_to_dataset_dir = working_dir + "/../data/datasets/"
-datasets = {"amazon", "movielens", "movietweetings"}
-# datasets = {"movietweetings"}
+path_to_mappings_dir = working_dir + "/../data/semantic_mappings/"
+
+# datasets = {"amazon", "movielens", "movietweetings"}
+datasets = {"movietweetings"}
+
 for dataset in datasets:
     # Load the dataset
     loader = recsys_loader(dataset)
     data = loader.load_dataset(path_to_dataset_dir)
     print data
+
+    # load the category mapping
+    item_categories = loader.load_item_category_mappings(path_to_mappings_dir)
+    print "Categories=" + str(len(item_categories))
 
     # Split the dataset
     splitter = datasetsplitter()
@@ -32,8 +40,13 @@ for dataset in datasets:
     rmse = sgd.test_model(svd_trained, splits.test)
     print "SVD = " + str(rmse)
 
-    svdplusplus = SVDPlusPlus(params, splits.train)
-    svdplusplus_trained = sgd.train_model(svdplusplus, splits.train)
-    rmse_svdplusplus = sgd.test_model(svdplusplus_trained, splits.test)
-    print "SVD Plus Plus = " + str(rmse_svdplusplus)
+    # svdplusplus = SVDPlusPlus(params, splits.train)
+    # svdplusplus_trained = sgd.train_model(svdplusplus, splits.train)
+    # rmse_svdplusplus = sgd.test_model(svdplusplus_trained, splits.test)
+    # print "SVD Plus Plus = " + str(rmse_svdplusplus)
+
+    itemcatssvd = ItemsCatsSVD(params, splits.train, item_categories)
+    itemcatssvd_trained = sgd.train_model(itemcatssvd, splits.train)
+    rmse_itemcatssvd = sgd.test_model(itemcatssvd_trained, splits.test)
+    print "SVD Items Cats = " + str(rmse_itemcatssvd)
 
